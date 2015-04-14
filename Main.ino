d07bf80dd3d37131476563e81778eccb5d72ecbb
SCMorley95 <SCMorley95@gmail.com>
2015-04-13 22:12:23 -0400

    Made rough stuff less rough
    StepperMotor is ~30% complete
    Main.ino is around the same

--- Main.ino
+++ Main.ino
@@ -1,9 +1,15 @@
 #include <QueueList.h> //download this!!
-//values for all of the cases
-//#define ... 0
-QueueList <int> NormalQueue;
-QueueList <int> PriorityQueue;
+#include <StepperMotor.h>
+#include <Serial.h>
+
+#define TURRET_DIR_FORWARDS true;
+#define TURRET_DIR_BACKWARDS false;
+
+boolean isTurretRotation = true;
+boolean turretDirection = TURRET_DIR_FORWARDS;
+
 void setup() {
+	StepperMotor turretMotor = new StepperMotor(STEP_PIN, DIR_PIN);
 	//instantiate Encoder module
 	//Gyro module
 	//PID module
@@ -15,25 +21,85 @@ void setup() {
 	//Display module
 }
 void loop() {
-	setObjective();
-	if(!PriorityQueue.isEmpty()) {
-		state = PriorityQueue.pop();
-	}
-	else if(!PriorityQueue.isEmpty()) {
-		state = NormalQueue.pop();
-	}
-	else {
-		state = default;
-	}
+	state = setObjective();
 	switch(state) {
 		case default:
 			break;
-		case 0:
-		case 1:
-		case 2:
+		case 0: //about to go off cliff
+			stopRobot();
+			moveFwd(-10);
+			turnDeg(-90);
+			break;
+		case 1: //flame detected
+			stopRobot();
+			isTurretRotation = false;
+			while(!flameOut()) {
+				fanMotor.write(FAN_FULL_ON);
+			}
+			reportLocation();
+			break;
+		case 2: //walls on front and right
+			turnDeg(-90);
+			break;
+		case 3: //wall just on right
+			setRobotSpeed(ROBOT_FULL_FORWARD);
+			break;
+		case 4: //wall just in front
+			turnDeg(90);
+			break;
+		case 5: //no walls detected
+			turnDeg(90);
+			break;
 		//etc
 	}
 }
-void setObjective() {
-
+int setObjective() {
+	if(!checkLine()) {
+		return 0;
+	}
+	if(isFlame()) {
+		return 1;
+	}
+	else if(wallSide()) {
+		if(wallFront()) {
+			return 2;
+		}
+		else {
+			return 3;
+		}
+	}
+	else {
+		if(wallFront()) {
+			return 4;
+		}
+		else {
+			return 5;
+		}
+	}
+}
+boolean checkLine() {
+	//check the line sensor to detect if we are over a black line ( black line == cliff == SCARY!!!!!)
+}
+boolean wallFront() {
+	//check the ultrasonic sensor to see if the detected distance is under (insert appropriate comparator here)
+}
+boolean wallSide() {
+	//check the LIDAR to see if (something something LIDAR something something wall on the right)
+}
+boolean isFlame() {
+	//look real hard for a flame
+}
+boolean flameOut() {
+	//look slightly less hard for a flame (or if that ends up not working just slap a timer up in this)
+}
+void turretISR() {
+	if(isTurretRotation) {
+		turretMotor.step(turretDirection);
+		if(turretMotor.getPos() = 180) {
+			turretDirection = TURRET_DIR_BACKWARDS;
+		}
+		else if(turretMotor.getPos() = 0) {
+			turretDirection = TURRET_DIR_FORWARDS;
+		}
+	}
 }
